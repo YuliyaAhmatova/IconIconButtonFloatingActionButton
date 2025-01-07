95b6b81f7192d7d68f25400e716f3e12f20cb563
Yuliya Ahmatova <ahmatovay@mail.ru>
2025-01-08 01:14:47 +0500

    ДЗ AlertDialog

--- app/src/main/java/com/example/iconiconbuttonfloatingactionbutton/MainActivity.kt
+++ app/src/main/java/com/example/iconiconbuttonfloatingactionbutton/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.iconiconbuttonfloatingactionbutton
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -17,139 +19,216 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            NotesApp()
+        }
+    }
+}
 
-            val input = rememberSaveable { mutableStateOf("") }
-            val notesList = rememberSaveable { mutableListOf<String>() }
-            // Преобразуем его в SnapshotStateList для использования в Compose
-            val notesStateList = remember { mutableStateListOf<String>() }
+@Composable
+fun NotesApp() {
+    val input = rememberSaveable { mutableStateOf("") }
+    val notesList = rememberSaveable { mutableListOf<String>() }
+    val notesStateList = remember { mutableStateListOf<String>() }
+    var openDialog by remember { mutableStateOf<Pair<Boolean, String?>>(false to null) }
 
-            // Обновляем notesStateList при изменении notesList
-            notesStateList.clear()
-            notesStateList.addAll(notesList)
+    notesStateList.clear()
+    notesStateList.addAll(notesList)
 
-            Box(
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .background(color = Color.LightGray, shape = RoundedCornerShape(8.dp))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "Заметки",
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(8.dp)
-                    .background(color = Color.LightGray, shape = RoundedCornerShape(8.dp))
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
+                    .fillMaxWidth()
+                    .background(
+                        color = Color.DarkGray,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(12.dp),
+                fontWeight = FontWeight.Bold,
+                fontSize = 26.sp,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+            OutlinedTextField(
+                value = input.value,
+                onValueChange = { input.value = it },
+                textStyle = TextStyle(fontSize = 22.sp, textAlign = TextAlign.Center),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.White, shape = RoundedCornerShape(8.dp)),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Black,
+                    unfocusedBorderColor = Color.Black
+                ),
+                placeholder = {
                     Text(
-                        text = "Заметки",
+                        text = "Введите текст...",
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .background(
-                                color = Color.DarkGray,
-                                shape = RoundedCornerShape(8.dp)
-                            )
-                            .padding(12.dp),
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 26.sp,
-                        color = Color.White,
+                            .fillMaxWidth(),
+                        fontSize = 22.sp,
                         textAlign = TextAlign.Center
                     )
-                    OutlinedTextField(
-                        value = input.value,
-                        onValueChange = { input.value = it },
-                        textStyle = TextStyle(fontSize = 22.sp, textAlign = TextAlign.Center),
+                }
+            )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .padding(4.dp)
+            ) {
+                items(notesStateList) { item ->
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier
                             .fillMaxWidth()
-                            .background(color = Color.White, shape = RoundedCornerShape(8.dp)),
-                        colors = OutlinedTextFieldDefaults.colors(
-                            focusedBorderColor = Color.Black,
-                            unfocusedBorderColor = Color.Black
-                        ),
-                        placeholder = {
-                            Text(
-                                text = "Введите текст...",
-                                modifier = Modifier
-                                    .fillMaxWidth(),
-                                fontSize = 22.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                    )
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(1f)
                             .padding(4.dp)
+                            .background(
+                                color = Color.White,
+                                shape = RoundedCornerShape(20.dp)
+                            )
+                            .padding(
+                                top = 4.dp,
+                                bottom = 4.dp,
+                                start = 15.dp,
+                                end = 15.dp
+                            ),
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        items(notesStateList) { item ->
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(4.dp)
-                                    .background(
-                                        color = Color.White,
-                                        shape = RoundedCornerShape(20.dp)
-                                    )
-                                    .padding(
-                                        top = 4.dp,
-                                        bottom = 4.dp,
-                                        start = 15.dp,
-                                        end = 15.dp
-                                    ),
-                                horizontalArrangement = Arrangement.SpaceBetween
-                            ) {
-                                Text(
-                                    text = item,
-                                    fontSize = 20.sp
-                                )
-                                IconButton(
-                                    onClick = {
-                                        notesList.remove(item)
-                                        notesStateList.clear()
-                                        notesStateList.addAll(notesList)
-                                    }
-                                ) {
-                                    Icon(Icons.Default.Delete, "", tint = Color.DarkGray)
-                                }
+                        Text(
+                            text = item,
+                            fontSize = 20.sp
+                        )
+                        IconButton(
+                            onClick = {
+                                openDialog = true to item
                             }
+                        ) {
+                            Icon(Icons.Default.Delete, "", tint = Color.DarkGray)
                         }
                     }
                 }
-                FloatingActionButton(
-                    modifier = Modifier
-                        .align(alignment = Alignment.BottomEnd)
-                        .padding(8.dp),
-                    onClick = {
-                        if (input.value.isNotBlank()) {
-                            notesList.add(input.value)
-                            input.value = ""
-                        }
-                    },
-                    contentColor = Color.White,
-                    containerColor = Color.DarkGray
+            }
+        }
+        FloatingActionButton(
+            modifier = Modifier
+                .align(alignment = Alignment.BottomEnd)
+                .padding(8.dp),
+            onClick = {
+                if (input.value.isNotBlank()) {
+                    notesList.add(input.value)
+                    input.value = ""
+                }
+            },
+            contentColor = Color.White,
+            containerColor = Color.DarkGray
+        ) {
+            Icon(Icons.Filled.Add, "")
+        }
+    }
+    if (openDialog.first) {
+        DialogWithImage(
+            onDismissRequest = { openDialog = false to null },
+            onConfirmation = {
+                notesList.remove(openDialog.second)
+                notesStateList.clear()
+                notesStateList.addAll(notesList)
+                openDialog = false to null
+            },
+            painter = painterResource(id = R.drawable.delete),
+            description = "Подтверждение удаления"
+        )
+    }
+}
+
+@Composable
+fun DialogWithImage(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    painter: Painter,
+    description: String
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(375.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = description,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.height(160.dp)
+                )
+                Text(
+                    text = "Подтвердите удаление элемента",
+                    modifier = Modifier.padding(16.dp)
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Icon(Icons.Filled.Add, "")
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = "Отмена")
+                    }
+                    TextButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(text = "Удалить")
+                    }
                 }
             }
         }
